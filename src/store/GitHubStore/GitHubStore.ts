d2af4f1e879e08956cae336e59157eefe68d6b0d
Ilya Konik <konick.ilya@yandex.ru>
2022-02-11 15:19:54 +0500

    Comments deleted, post request updated

--- src/store/GitHubStore/GitHubStore.ts
+++ src/store/GitHubStore/GitHubStore.ts
@@ -9,16 +9,11 @@ import {
 } from "./types";
 
 export default class GitHubStore implements IGitHubStore {
-  private readonly apiStore = new ApiStore("http://api.github.com"); // TODO: не забудьте передать baseUrl в конструктор
-
-  // TODO: реализовать интерфейс IGitHubStore
+  private readonly apiStore = new ApiStore("http://api.github.com");
 
   async getOrganizationReposList(
     params: GetOrganizationReposListParams
   ): Promise<ApiResp<RepoItem[]>> {
-    // TODO: Здесь сделайте вызов из this.apiStore и верните результат
-    // Документация github: https://docs.github.com/en/rest/reference/repos#list-organization-repositories
-
     return this.apiStore.request({
       headers: { Accept: "*/*" },
       endpoint: `/orgs/${params.organizationName}/repos`,
@@ -30,9 +25,10 @@ export default class GitHubStore implements IGitHubStore {
   createRepo(params: CreateRepoParams): Promise<ApiResp<string>> {
     return this.apiStore.request({
       headers: {
-        Authorization: "Basic " + params.token,
+        Authorization: "token " + params.token,
+        accept: "application/vnd.github.v3+json",
       },
-      endpoint: "",
+      endpoint: "/user/repos",
       data: { name: params.repoName },
       method: HTTPMethod.POST,
     });
