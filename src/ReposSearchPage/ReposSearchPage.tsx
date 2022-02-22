ebc28fd49d1e8f3c72701532e0b6fb84919c7ab0
Ilya Konik <konick.ilya@yandex.ru>
2022-02-22 18:18:46 +0500

    RepoBranchesDrawer component added to layout

--- src/ReposSearchPage/ReposSearchPage.tsx
+++ src/ReposSearchPage/ReposSearchPage.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 import Button from "@components/Button";
 import Input from "@components/Input";
+import RepoBranchesDrawer from "@components/RepoBranchesDrawer";
 import RepoTile from "@components/RepoTile";
 import SearchIcon from "@components/SearchIcon";
 import GitHubStore from "@store/GitHubStore";
@@ -14,8 +15,16 @@ const ReposSearchPage = () => {
   const [inputValue, setInputValue] = useState("");
   const [isLoading, setLoading] = useState(false);
   const [repos, setRepos] = useState<any[]>([]);
+  const [isDrawerVisible, setDrawersVisible] = useState(false);
+  const [selectedRepo, setSelectedRepo] = useState(null);
 
-  useEffect(() => {}, []);
+  const onClose = () => {
+    setDrawersVisible(false);
+  };
+
+  const showDrawer = () => {
+    setDrawersVisible(true);
+  };
 
   const loadRepos = (e: React.MouseEvent): void => {
     setLoading(true);
@@ -35,27 +44,41 @@ const ReposSearchPage = () => {
   };
 
   return (
-    <div className="repo-search-page">
-      <Input
-        value={inputValue}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          setInputValue(e.target.value);
-        }}
-        placeholder={"Введите название организации"}
+    <React.Fragment>
+      <div className="repo-search-page">
+        <Input
+          value={inputValue}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setInputValue(e.target.value);
+          }}
+          placeholder={"Введите название организации"}
+        />
+        <Button
+          onClick={loadRepos}
+          children={<SearchIcon currentColor={"#FFFFFF"} />}
+          disabled={isLoading}
+        />
+        {repos.length !== 0 && (
+          <div className="repo-search-page__container">
+            {repos.map((repoItem) => (
+              <RepoTile
+                item={repoItem}
+                key={repoItem.id}
+                onClick={() => {
+                  setSelectedRepo(repoItem);
+                  showDrawer();
+                }}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+      <RepoBranchesDrawer
+        onClose={onClose}
+        isDrawerVisible={isDrawerVisible}
+        selectedRepo={selectedRepo}
       />
-      <Button
-        onClick={loadRepos}
-        children={<SearchIcon currentColor={"#FFFFFF"} />}
-        disabled={isLoading}
-      />
-      {repos.length !== 0 && (
-        <div className="repo-search-page__container">
-          {repos.map((repoItem) => (
-            <RepoTile item={repoItem} key={repoItem.id} onClick={() => {}} />
-          ))}
-        </div>
-      )}
-    </div>
+    </React.Fragment>
   );
 };
 
