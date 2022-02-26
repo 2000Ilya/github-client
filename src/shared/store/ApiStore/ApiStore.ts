fcad8b99efd415d717e5530465e42b91547ed739
Ilya Konik <konick.ilya@yandex.ru>
2022-02-26 11:43:49 +0500

    let vars changed to const

--- src/shared/store/ApiStore/ApiStore.ts
+++ src/shared/store/ApiStore/ApiStore.ts
@@ -18,16 +18,16 @@ export default class ApiStore implements IApiStore {
   async request<SuccessT, ErrorT = any, ReqT = {}>(
     params: RequestParams<ReqT>
   ): Promise<ApiResponse<SuccessT, ErrorT>> {
-    let url = `${this.baseUrl}${params.endpoint}${
+    const url = `${this.baseUrl}${params.endpoint}${
       params.method === HTTPMethod.GET ? `?${qs.stringify(params.data)}` : ""
     }`;
 
-    let commonRequestParams = {
+    const commonRequestParams = {
       headers: params.headers,
       method: params.method,
     };
 
-    let requestParams =
+    const requestParams =
       params.method === HTTPMethod.GET
         ? commonRequestParams
         : {
@@ -36,7 +36,7 @@ export default class ApiStore implements IApiStore {
           };
 
     try {
-      let response = await fetch(url, requestParams);
+      const response = await fetch(url, requestParams);
 
       if (response.ok) {
         return {
