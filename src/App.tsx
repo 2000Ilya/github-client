5c2e5d884e892f130d9f9e188628654d57928f85
Ilya Konik <konick.ilya@yandex.ru>
2022-03-16 11:21:06 +0500

    Adding context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 
 import { RepoItem } from "@store/GitHubStore/types";
+import gitHubStore from "@store/gitHubStoreInstance";
 import {
   BrowserRouter,
   Redirect,
@@ -14,35 +15,62 @@ import "./App.css";
 import RepoInfoPage from "./RepoInfoPage";
 import ReposSearchPage from "./ReposSearchPage";
 
-const App = () => {
-  const [isDrawerVisible, setDrawerVisible] = useState(false);
-  const [selectedRepo, setSelectedRepo] = useState<RepoItem | null>(null);
-  const history = useHistory();
+type ReposContextType = {
+  list: RepoItem[];
+  isLoading: boolean;
+  load: (repoOwner: string) => void;
+};
 
-  const onClose = (history: any) => {
-    setDrawerVisible(false);
-    // eslint-disable-next-line no-console
-    console.log(history);
-    history.push("/repos");
+const ReposContext = createContext<ReposContextType>({
+  list: [],
+  isLoading: true,
+  load: () => {},
+});
+
+const Provider = ReposContext.Provider;
+export const Consumer = ReposContext.Consumer;
+
+export const useReposContext = () => useContext(ReposContext);
+
+const App = () => {
+  const [list, setList] = useState<RepoItem[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const load = async (repoOwner: string) => {
+    try {
+      await gitHubStore
+        .getOrganizationReposList({
+          organizationName: repoOwner,
+          queryParameters: {},
+        })
+        .then((result) => {
+          setList(result.success ? result.data : []);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error(err);
+    }
   };
 
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/repos">
-            <ReposSearchPage
-              setDrawersVisible={setDrawerVisible}
-              setSelectedRepo={setSelectedRepo}
-            />
-          </Route>
-          <Route path="/repos/:id">
-            <RepoInfoPage selectedRepo={selectedRepo} />
-          </Route>
-          <Redirect to="/repos" />
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <Provider value={{ list, isLoading, load }}>
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/repos">
+              <ReposSearchPage />
+            </Route>
+            <Route path="/repos/:id">
+              <RepoInfoPage />
+            </Route>
+            <Redirect to="/repos" />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </Provider>
   );
 };
 
