af36518dcd2be20f7ed492f7d1a3493be223dbbd
Ilya Konik <konick.ilya@yandex.ru>
2022-03-16 11:24:14 +0500

    Adding context provider and consumer

--- src/ReposSearchPage/ReposSearchPage.tsx
+++ src/ReposSearchPage/ReposSearchPage.tsx
@@ -1,79 +1,57 @@
-import React, { useEffect, useState } from "react";
+import { useState } from "react";
 
 import Button from "@components/Button";
 import Input from "@components/Input";
-import RepoBranchesDrawer from "@components/RepoBranchesDrawer";
 import RepoTile from "@components/RepoTile";
 import SearchIcon from "@components/SearchIcon";
 import "./ReposSearchPage.css";
-import { RepoItem } from "@store/GitHubStore/types";
-import gitHubStore from "@store/gitHubStoreInstance";
-import { Redirect, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
-type ReposSearchPageProps = {
-  setDrawersVisible: (isDrawerVisible: boolean) => void;
-  setSelectedRepo: (selectedRepo: RepoItem | null) => void;
-};
+import { Consumer, useReposContext } from "../App";
 
-const ReposSearchPage: React.FC<ReposSearchPageProps> = ({
-  setDrawersVisible,
-  setSelectedRepo,
-}) => {
+const ReposSearchPage = () => {
+  const reposContext = useReposContext();
   const [inputValue, setInputValue] = useState("");
-  const [isLoading, setLoading] = useState(false);
-  const [repos, setRepos] = useState<RepoItem[]>([]);
   const history = useHistory();
-
-  const showDrawer = () => {
-    setDrawersVisible(true);
-  };
 
   const handleInputChange = (value: string) => {
     setInputValue(value);
   };
 
-  const fetchReposData = async () => {
-    const result = await gitHubStore.getOrganizationReposList({
-      organizationName: inputValue,
-      queryParameters: {},
-    });
-    setRepos(result.success ? result.data : []);
-  };
-
-  const loadRepos = async (e: React.MouseEvent) => {
-    setLoading(true);
-    await fetchReposData();
-    setLoading(false);
+  const loadReposData = () => {
+    reposContext.load(inputValue);
   };
 
   return (
-    <>
-      <div className="repo-search-page">
-        <Input
-          value={inputValue}
-          onChange={handleInputChange}
-          placeholder={"Введите название организации"}
-        />
-        <Button onClick={loadRepos} disabled={isLoading}>
-          <SearchIcon currentColor={"#FFFFFF"} />
-        </Button>
-        {!!repos.length && (
-          <div className="repo-search-page__container">
-            {repos.map((repoItem) => (
-              <RepoTile
-                item={repoItem}
-                key={repoItem.id}
-                onClick={() => {
-                  setSelectedRepo(repoItem);
-                  history.push(`/repos/${repoItem.id}`);
-                  showDrawer();
-                }}
-              />
-            ))}
+    <Consumer>
+      {(context) => (
+        <>
+          <div className="repo-search-page">
+            <Input
+              value={inputValue}
+              onChange={handleInputChange}
+              placeholder={"Введите название организации"}
+            />
+            <Button onClick={loadReposData} disabled={context.isLoading}>
+              <SearchIcon currentColor={"#FFFFFF"} />
+            </Button>
+            {!!context.list.length && (
+              <div className="repo-search-page__container">
+                {context.list.map((repoItem) => (
+                  <RepoTile
+                    item={repoItem}
+                    key={repoItem.id}
+                    onClick={() => {
+                      history.push(`/repos/${repoItem.id}`);
+                    }}
+                  />
+                ))}
+              </div>
+            )}
           </div>
-        )}
-      </div>
-    </>
+        </>
+      )}
+    </Consumer>
   );
 };
 
